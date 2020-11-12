e1bb31b4839a9d831dbe6a280c196426d46a00f3
Akash Das <aakashdas368@gmail.com>
2020-11-12 08:09:47 +0530

    show content links in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,42 @@
+import fs from "fs";
+import { GetStaticProps } from "next";
+import Link from "next/link";
+import path from "path";
 import React from "react";
 
-const IndexPage: React.FC = () => {
+interface Props {
+  files: string[];
+}
+
+const IndexPage: React.FC<Props> = (props: Props) => {
   return (
     <div>
       <h1>Presenter</h1>
+
+      <pre>
+        {props.files.map((file, idx) => (
+          <div key={idx}>
+            <Link href={`${file.replace(".md", "")}`}>
+              <a>{file.replace(".md", "")}</a>
+            </Link>
+          </div>
+        ))}
+      </pre>
     </div>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (): Promise<{
+  props: Props;
+}> => {
+  const contentsDirPath = path.join("src", "contents");
+  const files = fs.readdirSync(contentsDirPath);
+
+  return {
+    props: {
+      files: files,
+    },
+  };
 };
 
 export default IndexPage;
